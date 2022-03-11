5f5fd86d7a1b022a94ef1fdab2d4db7a0a2e25df
Xphalt <41804445+Xphalt@users.noreply.github.com>
2022-03-11 01:22:18 +0000

    Added float scriptable object variable & reference

---
+++ Assets/ScriptableObjects/Variables/ReferenceScripts/FloatReference.cs
@@ -0,0 +1,15 @@
+namespace ModularCraftingSystem
+{
+    [System.Serializable]
+    public class FloatReference
+    {
+        public bool useConstant = true;
+        public float constantValue;
+        public FloatVariable variable;
+
+        public float Value
+        {
+            get { return useConstant ? constantValue : variable.value; }
+        }
+    }
+}
---
+++ Assets/ScriptableObjects/Variables/VariableScripts/FloatVariable.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace ModularCraftingSystem
+{
+    [CreateAssetMenu(menuName = "Modular Crafting System/Data/Variables/Float", fileName = "NewFloatVariable")]
+    public class FloatVariable : ScriptableObject
+    {
+        public float value;
+    }
+}
