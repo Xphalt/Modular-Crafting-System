d2214ca934291bb857b314e8e1295f8eda0235ae
Linden Woodford <41804445+Xphalt@users.noreply.github.com>
2022-04-29 15:34:33 +0100

    Fixed recipes not being looped through

--- Packages/ModularCraftingSystem/Runtime/CraftingSystem/CraftingSystem.cs
+++ Packages/ModularCraftingSystem/Runtime/CraftingSystem/CraftingSystem.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace ModularCraftingSystem
@@ -9,76 +10,45 @@ namespace ModularCraftingSystem
         public Transform craftingSlotTransform;
         public InventorySlotUI outputSlot;
         [SerializeField] private int gridSize;
-        [SerializeField] private List<InventorySlotUI> listOfinventorySlotUIs;
+        [SerializeField] private List<InventorySlotUI> listOfSlots;
         [SerializeField] private List<CraftingRecipeData> listOfRecipes;
-        //public CraftingRecipeData recipe;
 
         public int GetGridSize => gridSize;
 
         private void Awake()
         {
-            listOfinventorySlotUIs = new List<InventorySlotUI>();
+            listOfSlots = new List<InventorySlotUI>();
 
             for (int i = 0; i < craftingSlotTransform.childCount; i++)
             {
-                listOfinventorySlotUIs.Add(craftingSlotTransform.GetChild(i).GetComponent<InventorySlotUI>());
+                listOfSlots.Add(craftingSlotTransform.GetChild(i).GetComponent<InventorySlotUI>());
             }
 
-            gridSize = listOfinventorySlotUIs.Count;
+            gridSize = listOfSlots.Count;
         }
 
         public void Update()
         {
-            InventoryResourceData craftingResult = null;
+            List<InventoryResourceData> listOfGridItem = new List<InventoryResourceData>();
 
-            craftingResult = LoopThroughRecipes();
-            CreateOutput(craftingResult);
+            foreach (InventorySlotUI slot in listOfSlots)
+            {
+                listOfGridItem.Add(slot.GetAssignedInventorySlot.GetItemData);
+            }
+
+            foreach (CraftingRecipeData recipe in listOfRecipes)
+            {
+                if (listOfGridItem.SequenceEqual(recipe.listOfComponents))
+                {
+                    CreateOutput(recipe.outputItem);
+                }
+            }
         }
 
         public void CreateOutput(InventoryResourceData craftingResult)
         {
             outputSlot.GetAssignedInventorySlot.UpdateInventorySlot(craftingResult, 1);
             outputSlot.UpdateUISlot();
-            
-        }
-
-        private InventoryResourceData LoopThroughRecipes()
-        {
-            InventoryResourceData craftingResult = null;
-
-            foreach (CraftingRecipeData recipe in listOfRecipes)
-            {
-                craftingResult = LoopThroughSlots(recipe);
-
-                if (craftingResult == null) { break; }
-            }
-
-            return craftingResult;
-        }
-
-        private InventoryResourceData LoopThroughSlots(CraftingRecipeData recipe)
-        {
-            InventoryResourceData craftingResult = null;
-
-            for (int i = 0; i < gridSize; i++)
-            {
-                craftingResult = DoesSlotItemMatchRecipe(recipe, i);
-
-                if (!craftingResult) { return null; }
-            }
-
-            return craftingResult;
-        }
-
-        private InventoryResourceData DoesSlotItemMatchRecipe(CraftingRecipeData recipe, int i)
-        {
-            if (/*listOfinventorySlotUIs.ToArray()[i].GetAssignedInventorySlot.GetItemData != null &&*/
-                listOfinventorySlotUIs.ToArray()[i].GetAssignedInventorySlot.GetItemData != recipe.listOfComponents.ToArray()[i])
-            {
-                return null;
-            }
-
-            return recipe.GetOutput();
         }
     }
 }
